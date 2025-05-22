cfd43152d74deed8e1748276cd9d9946cbac4e0d
Ex1skeleton miezlip <panjulskrebek@gmail.com>
2025-05-22 15:10:43 +0000

    make bot discord online and add command hai for testing

---
+++ commands/hai.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+import discord
+
+class Hai(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def hai(self, ctx):
+        embed = discord.Embed(
+            title="Halo!",
+            description="Hai saya Firefly, pembantu Anda. Apa yang harus saya lakukan?",
+            color=int("303135", 16)
+        )
+        await ctx.reply(embed=embed, mention_author=True)
+
+# Required setup function for extension loading
+async def setup(bot):
+    await bot.add_cog(Hai(bot))
---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+# Load token in .env
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+# required intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+# commands bot
+client = commands.Bot(command_prefix = '?', intents=intents)
+
+# make bot online
+@client.event
+async def on_ready():
+    print("bot ready online")
+    print("================")
+
+# Load commands from folder
+@client.event
+async def setup_hook():
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"commands.{filename[:-3]}")
+
+# run bot
+client.run(TOKEN)
