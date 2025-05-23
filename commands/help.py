60b5339cc3c46809bfaf4ae291333668652ce5da
Ex1skeleton miezlip <panjulskrebek@gmail.com>
2025-05-23 00:58:58 +0000

    update embed message add footer in help & field/stimestamp etc.

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from datetime import datetime
 
 class Help(commands.Cog):
     def __init__(self, bot):
@@ -7,11 +8,24 @@ class Help(commands.Cog):
 
     @commands.command(name="help")
     async def help(self, ctx):
+        guild = ctx.guild
         embed = discord.Embed(
-            title="Help!",
-            description="**!hai** - say hi for firefly",
+            title="List Commands",
             color=3158325,
+            timestamp=datetime.utcnow()
         )
+        # field
+        embed.add_field(
+            name="General",
+            value="**!hai** - say hi for firefly\n**!help** - show list cmd (test)",
+            inline=False
+        )
+        # footer
+        embed.set_footer(
+                text=guild.name,
+                icon_url=guild.icon.url if guild.icon else None
+            )
+
         await ctx.reply(embed=embed, mention_author=True)
 
 # Required setup function for extension loading
