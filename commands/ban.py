748fe4b436334f0c3bb6395cb3e63a6003c6208a
farelfair <panjulskrebek@gmail.com>
2025-05-23 22:35:36 +0800

    switch to embed message when bot replies to user

--- commands/ban.py
+++ commands/ban.py
@@ -7,21 +7,31 @@ class Ban(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, member: discord.Member, *, reason="Tidak ada alasan diberikan"):
+    async def ban(self, ctx, member: discord.Member, *, reason="no reason given"):
         try:
             await member.ban(reason=reason)
             embed = discord.Embed(
-                title="<a:banned:755637224854388809> Member Hitam out aja",
-                description=f"{member.mention} sok asik jawa out aja sana",
-                color=int("303135", 16)
+                title="Banned Member",
+                description=f"{member.mention}",
+                color=3158325,
             )
             embed.add_field(name="Alasan", value=reason, inline=False)
             embed.set_footer(text=f"Diban oleh: {ctx.author}", icon_url=ctx.author.avatar.url if ctx.author.avatar else None)
             await ctx.send(embed=embed)
         except discord.Forbidden:
-            await ctx.send("❌ Saya tidak punya izin untuk memban member ini.")
+            embed = discord.Embed(
+                title="Permission not allowed",
+                description="firefly does not have permission",
+                color=3158325,
+            )
+            await ctx.send(embed=embed)
         except Exception as e:
-            await ctx.send(f"⚠️ Terjadi error: {e}")
+            embed = discord.Embed(
+                title="Error Detected",
+                description=f"an error occurred: {e}",
+                color=3158325,
+            )
+            await ctx.send(embed=embed)
 
     @ban.error
     async def ban_error(self, ctx, error):
